fc0d4aa6326b7c3bbb9717813d3b7d7acba3fca4
Yashas Gowda <yashasgowda270@outlook.com>
2021-06-18 17:56:00 +0530

    Template ready.

--- src/ai-projects.ts
+++ src/ai-projects.ts
@@ -5,74 +5,9 @@ import { customElement, property } from 'lit/decorators.js';
 export class AiProjects extends LitElement {
     @property({ type: String }) title = 'My app';
 
-    static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--ai-projects-background-color);
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo > svg {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
-    }
-  `;
+    static styles = css``;
 
     render() {
-        return html`
-      <main>
-        <h1>${this.title}</h1>
-
-        <p>Edit <code>src/AiProjects.ts</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
-    `;
+        return html``;
     }
 }
